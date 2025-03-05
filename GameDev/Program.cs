f7f8d74c488de2a5b4c771d88c5393d3322d4a9d
Bruno Carvalho <brunocarv4519@gmail.com>
2025-03-05 11:47:40 -0300

    Adicionar arquivos de projeto.

---
+++ GameDev/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Digite um nome para o seu personagem:");
+string nameUser = (Console.ReadLine());
+Console.WriteLine("Escolha o número da classe de seu personagem:\n1.Guerreiro\n2.Mago");
+int seresMisticos = int.Parse(Console.ReadLine());
+
+switch (seresMisticos)
+{
+    case 1:
+        string nameOfClass = "Guerreiro";
+        int life = 100;
+        int mana = 40;
+        int ataqueNormal = 10;
+        int ataqueEspecial = 30;
+        Console.WriteLine($"Olá {nameUser}, você escolheu a classe {nameOfClass}.\nSeus atributos são:\nVida: {life}\nMana: {mana}\nPoder: {ataqueEspecial + ataqueNormal}");
+        break;
+    case 2:
+        string nameOfClass2 = "Mago";
+        int life2 = 60;
+        int mana2 = 100;
+        int ataqueNormal2 = 15;
+        int ataqueEspecial2 = 40;
+        Console.WriteLine($"Olá {nameUser}, você escolheu a classe {nameOfClass2}.\nSeus atributos são:\nVida: {life2}\nMana: {mana2}\nPoder: {ataqueEspecial2 + ataqueNormal2}");
+        break;
+    default:
+        Console.WriteLine("Opção inválida");
+        break;
+}
---
+++ GameDev/README.txt
@@ -0,0 +1,12 @@
+Desenvolvimento do Jogo:
+
+CRIAÇÃO DO PERSONAGEM
+- O jogo deve conter o nome do player, fornecido pelo usuário.                                                            (OK)
+- Deve oferecer um sistema de classes ao jogador (Começar com mago e guerreiro).                                          (OK)
+- Os atributos de classe devem ser Força, Inteligencia. (Por enquanto vida e mana)                                        (OK)
+- As informações do personagem devem ser fornecidas ao jogador após a criação e escolha do tipo de personagem.            (OK)
+- Trocar escolha de classes por um Switch Case.                                                                           (OK)
+- Criar poder de ataque para cada classe.
+
+PRIMEIRA BATALHA
+- Será necessário criar um poder de ataque para cada classe.
