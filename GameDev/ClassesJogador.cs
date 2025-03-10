8e6c9c94a560f915214ccfe79908c5e888508bc3
Bruno Carvalho <brunocarv4519@gmail.com>
2025-03-10 20:53:54 -0300

    Criação de novas branchs no código.

---
+++ GameDev/ClassesJogador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDev
+{
+    class ClassesJogador
+    {
+        public static void classesDePersonagem(string nameUser, int seresMisticos) 
+        {
+            switch (seresMisticos)
+            {
+                case 1:
+                    Console.WriteLine("Você escolheu a classe Guerreiro");
+                    int vida1 = 100;
+                    int ataque1 = 10;
+                    int mana1 = 20;
+                    Console.WriteLine($"Seus atributos iniciais são:\nVida: {vida1}\nAtaque: {ataque1}\nMana: {mana1}");
+                    break;
+                case 2:
+                    Console.WriteLine("Você escolheu a classe Mago");
+                    int vida2 = 70;
+                    int ataque2 = 12;
+                    int mana2 = 50;
+                    Console.WriteLine($"Seus atributos iniciais são:\nVida: {vida2}\nAtaque: {ataque2}\nMana: {mana2}");
+                    break;
+            }
+            }
+    }
+}
---
+++ GameDev/Dungeons.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDev
+{
+    class Dungeons
+    {
+        public static void DungeonsDoJogo(int opcaoDeEntrada)
+        {
+            if (opcaoDeEntrada == 1)
+            {
+                Console.WriteLine("Você entrou na dungeon");
+            }
+            else
+            {
+                Console.WriteLine("Você saiu do jogo");
+            }
+        }
+    }
+}
